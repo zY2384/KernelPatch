8c33e912b6f7197a6ea789b14cf2f42f49c181eb
zY2384 <2597665307@qq.com>
2026-09-06 03:11:55 +0800

    hello

--- lkm/include/kp_lkm.h
+++ lkm/include/kp_lkm.h
@@ -7,7 +7,7 @@
 #ifndef _KP_LKM_H_
 #define _KP_LKM_H_
 
-#include "log.h"
+#include <linux/printk.h>
 #include <linux/version.h>
 
 /* Version. Kbuild reads the repo-root ../version file and passes the three
@@ -37,5 +37,26 @@ static inline long kp_kver(void)
 	return (long)LINUX_VERSION_CODE;
 }
 
+//#define KP_LOG_ENABLE
+#define KPLKM_TAG "kernelpatch-lkm"
+#ifdef KP_LOG_ENABLE
+#define logki(fmt, ...) pr_info(KPLKM_TAG ": " fmt, ##__VA_ARGS__)
+#define logke(fmt, ...) pr_err(KPLKM_TAG ": " fmt, ##__VA_ARGS__)
+#define logkw(fmt, ...) pr_warn(KPLKM_TAG ": " fmt, ##__VA_ARGS__)
+#define logkd(fmt, ...) pr_debug(KPLKM_TAG ": " fmt, ##__VA_ARGS__)
+#define logkem(fmt, ...) pr_emerg(KPLKM_TAG ": " fmt, ##__VA_ARGS__)
+#define logkfi(fmt, ...) logki(fmt, ##__VA_ARGS__)
+#define logkfe(fmt, ...) logke(fmt, ##__VA_ARGS__)
+#define logkfd(fmt, ...) logkd(fmt, ##__VA_ARGS__)
+#else
+#define logki(fmt, ...) no_printk(fmt, ##__VA_ARGS__)
+#define logke(fmt, ...) no_printk(fmt, ##__VA_ARGS__)
+#define logkw(fmt, ...) no_printk(fmt, ##__VA_ARGS__)
+#define logkd(fmt, ...) no_printk(fmt, ##__VA_ARGS__)
+#define logkem(fmt, ...) no_printk(KPLKM_TAG ": " fmt, ##__VA_ARGS__)
+#define logkfi(fmt, ...) no_printk(fmt, ##__VA_ARGS__)
+#define logkfe(fmt, ...) no_printk(fmt, ##__VA_ARGS__)
+#define logkfd(fmt, ...) no_printk(fmt, ##__VA_ARGS__)
+#endif
 
 #endif /* _KP_LKM_H_ */
